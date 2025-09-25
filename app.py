64d7509e7492ea5775812d03ec87d81c5eeec5e3
0yaizu <kaira.oyaizu@gmail.com>
2025-09-25 10:13:26 +0900

    🎉 First Commit
    第1回、第2回授業まで

---
+++ app.py
@@ -0,0 +1,32 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = "onlineshop"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	username = None
+	return render_template("index.html", username=username)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	username = None
+	password = None
+	if request.method == "POST":
+		username = request.form.get("username")
+		password = request.form.get("password")
+		print(username)
+		print(password)
+		return redirect(url_for("index"))
+
+	return render_template("login.html", username=username, password=password)
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+	if request.method == "POST":
+		return redirect(url_for("index"))
+	return render_template("logout.html")
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=8080, debug=True)
