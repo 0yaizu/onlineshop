632d1735b933c0cf503f77866899a7050c8c85c8
0yaizu <kaira.oyaizu@gmail.com>
2025-10-09 10:14:07 +0900

    ✨ signupページを作成。

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from forms import *
 from models import *
 import os
@@ -49,6 +49,29 @@ def logout():
 		return redirect(url_for("index"))
 
 	return render_template("logout.html", form=form)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+	form = SignupForm()
+	if form.validate_on_submit():
+		username = form.username.data
+		password = form.password.data
+		confirmed_password = form.confirmed_password.data
+
+		user = da.search_user(username)
+		if user:
+			flash("This username is already used.", category="danger")
+			return redirect(url_for("signup"))
+
+		user = User()
+		user.username = username
+		user.password = generate_password_hash(password)
+		user = da.add_user(user)
+
+		flash("You are Sign Up.", category="success")
+		return redirect(url_for("index"))
+
+	return render_template("signup.html", form=form)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080, debug=True)
--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 # forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, length
+from wtforms.validators import DataRequired, length, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -25,10 +25,40 @@ class LoginForm(FlaskForm):
     self.password.data = user.password
 
   def copy_to(self, user):
-
     user.username = self.username.data
     user.password = self.password.data
 
-
 class LogoutForm(FlaskForm):
   submit = SubmitField("Logout")
+
+class SignupForm(FlaskForm):
+	username = StringField(
+		"User Name",
+		validators = [
+		DataRequired(message="User Name is required."),
+		length(max=64, message="User Name should be input within 64 characters."),
+		],
+	)
+	password = PasswordField(
+		"Password",
+		validators = [
+		DataRequired(message="Password is required."),
+		],
+	)
+	confirmed_password = PasswordField(
+		"Confirm Password",
+		validators=[
+		DataRequired(message="Confirmed Password is required."),
+		EqualTo("password", message="Passwords must match")
+		],
+	)
+	submit = SubmitField("Sign Up")
+
+	def copy_from(self, user):
+		self.username.data = user.username
+		self.password.data = user.password
+		self.confirmed_password.data = user.password
+
+	def copy_to(self, user):
+		user.username = self.username.data
+		user.password = self.password.data
